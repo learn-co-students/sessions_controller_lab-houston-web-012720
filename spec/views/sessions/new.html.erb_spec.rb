f0076dc7e510e39069419901924a92c8a21408fa
somaia-khalil <sumaya_khair@live.com>
2020-02-29 20:35:52 -0600

    Done.

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "sessions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+RSpec.describe SessionsController, type: :controller do
+  render_views
+  describe 'post create' do
+    it 'redirects to the login page if :name is nil' do
+      post :create
+      expect(response).to redirect_to controller: 'sessions', action: 'new'
+    end
+  end
 end
